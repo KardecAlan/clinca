4f58aa906f00e930528ce5d9f5e685ec5afb5441
Alan Kardec <george.jesus@equilibriumweb.com>
2024-01-26 23:35:03 -0300

    Criando a primeira Migrations com Flyway para a tabela medicos

--- src/main/java/com/kardec/clinica/medico/Medico.java
+++ src/main/java/com/kardec/clinica/medico/Medico.java
@@ -7,7 +7,7 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Table(name = "medicos")
+@Table(name = "MEDICOS")
 @Entity(name = "Medico")
 @Getter
 @NoArgsConstructor
