a4c3acb573f8145e67c0b544c77904318eedbc79
Alan Kardec <george.jesus@equilibriumweb.com>
2024-01-27 16:39:22 -0300

    Criando cadastro para pacientes

--- src/main/java/com/kardec/clinica/controller/PacienteController.java
+++ src/main/java/com/kardec/clinica/controller/PacienteController.java
@@ -1,14 +1,21 @@
 package com.kardec.clinica.controller;
 
 import com.kardec.clinica.paciente.DadosCadastroPaciente;
+import com.kardec.clinica.paciente.Paciente;
+import com.kardec.clinica.paciente.PacienteRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/pacientes")
 public class PacienteController {
 
+    @Autowired
+    private PacienteRepository repository;
+
     @PostMapping
-    public void cadastrarPacientes(@RequestBody DadosCadastroPaciente dados){
-        System.out.println("dados recebido: " +dados);
+    public void cadastrarPacientes(@RequestBody @Valid DadosCadastroPaciente dados){
+        repository.save(new Paciente(dados));
     }
 }
--- src/main/java/com/kardec/clinica/paciente/PacienteRepository.java
+++ src/main/java/com/kardec/clinica/paciente/PacienteRepository.java
@@ -1,2 +1,6 @@
-package com.kardec.clinica.paciente;public interface PacienteRepository {
+package com.kardec.clinica.paciente;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PacienteRepository extends JpaRepository<Paciente, Long> {
 }
