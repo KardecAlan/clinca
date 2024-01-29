5d1273606c2aac852b8c1e1794e5c263de167940
Alan Kardec <alankardec05@gmail.com>
2024-01-29 01:01:45 -0300

    Paginacao para pacientes

--- src/main/java/com/kardec/clinica/controller/PacienteController.java
+++ src/main/java/com/kardec/clinica/controller/PacienteController.java
@@ -1,10 +1,16 @@
 package com.kardec.clinica.controller;
 
+import com.kardec.clinica.medico.DadosListagemMedico;
 import com.kardec.clinica.paciente.DadosCadastroPaciente;
+import com.kardec.clinica.paciente.DadosListagemPaciente;
 import com.kardec.clinica.paciente.Paciente;
 import com.kardec.clinica.paciente.PacienteRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,7 +21,13 @@ public class PacienteController {
     private PacienteRepository repository;
 
     @PostMapping
+    @Transactional
     public void cadastrarPacientes(@RequestBody @Valid DadosCadastroPaciente dados){
         repository.save(new Paciente(dados));
+    }
+
+    @GetMapping
+    public Page<DadosListagemPaciente> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao){
+        return repository.findAll(paginacao).map(DadosListagemPaciente::new);
     }
 }
